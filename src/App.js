0bf4f20b843ccfa57d287e6641a12d15da95d447
akarenmartz <karenmartinez.mc11@gmail.com>
2023-07-25 17:24:14 -0600

    Se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState} from 'react';
+import axios from 'axios';
+//Components
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [movies, setListaMovies]=useState([]);
+  const [mostrar,setMostrar]=useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const nombre = e.target.elements.nombre.value;
+        if(nombre=== ''){
+            alert("Llena todos los campos");
+        }else{
+            const url = 'https://movie-database-alternative.p.rapidapi.com/?s='+nombre+'&r=json&page=1';
+            const obtenerMovies = async () => {
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'X-RapidAPI-Key': 'cdb4a96ef0mshbf1dd3838c9df27p170b34jsn293b2cfb02db',
+                        'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+                    }
+                };
+                const result = await axios.get(url,options)
+                setListaMovies(result.data)
+                const resulF = (result.data)
+                console.log(resulF);
+                if (resulF.Response === "True"){
+                    const listaMovie = resulF.Search;
+                    setListaMovies(listaMovie)
+                    setMostrar(true);
+                }else{
+                    setMostrar(false)
+                }
+            }
+            obtenerMovies();
+        }
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+        <div>
+                <form onSubmit={handleSubmit}>
+                    <div class="input-group mb-3" style={{padding:"20px"}}>
+                    <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Ingresa el nombre de la movie" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                        <div class="input-group-append" style={{padding:"8px"}}>
+                            <button  type='submit' className="btn btn-primary" >Buscar</button>
+                        </div>
+                    </div>
+                </form>
+        </div>
+        {mostrar ? 
+        <div className="container">
+            <div className="row">
+                {movies.map((item, index) => (
+                <div key={index} className="col-lg-3 col-md-6 col-sm-12 mb-4">
+                    <div className="card" style={{ minWidth: "200px" }}>
+                    <img className="card-img-top" src={item.Poster} alt="movies" />
+                    <div className="card-body">
+                        <h5 className="card-title">{item.Title}</h5>
+                        <hr />
+                        <p className="card-text">Año: {item.Year}</p>
+                    </div>
+                    </div>
+                </div>
+                ))}
+            </div>
+        </div>
+        :
+        <div className='container' style={{padding: "140px", marginLeft:"420px"}}>
+            <h3>No hay resultados</h3>
+        </div> }
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand text-uppercase" href="/">
+          Movies
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
